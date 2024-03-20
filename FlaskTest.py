2dd798d10c912c4e0feeb05dca9a2829f1d3120d
Kyle Seaford <147045805+KyleSeaford@users.noreply.github.com>
2024-03-20 12:52:24 +0000

    Update FlaskTest.py

--- FlaskTest.py
+++ FlaskTest.py
@@ -1,32 +1,38 @@
-import matplotlib.pyplot as plt
 from flask import Flask, render_template, jsonify
 import psutil
 import datetime
+import platform
 
 app = Flask(__name__)
 
-# function to get the system ram usage
 def get_ram_usage():
-    # Get the system memory usage in percentage
     memory_usage = psutil.virtual_memory().percent
     return memory_usage
 
-# app routes
+def get_system_name():
+    system_name = platform.system()
+    return system_name
+
+
+
+
+@app.route('/')
+def home():
+    system_name = get_system_name()
+    return render_template('index.html', name=system_name)
+
 @app.route('/ram_usage')
 def ram_usage():
     ram_usage = get_ram_usage()
     return jsonify({'ram_usage': ram_usage})
 
+@app.route('/current_date')
+def current_date():
+    return jsonify({'current_date': datetime.datetime.now().strftime("%Y-%m-%d")})
+
 @app.route('/current_time')
 def current_time():
-    return jsonify({'current_time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
-
-
-# main route
-@app.route('/')
-def home():
-    return render_template('index.html')
-
+    return jsonify({'current_time': datetime.datetime.now().strftime("%H:%M:%S")})
 
 if __name__ == '__main__':
     app.run(debug=True)
