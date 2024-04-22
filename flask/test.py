d8aff9c580497ebb39b1d7163bf4329f92060e9c
KyleSeaford <kyleseaford5@gmail.com>
2024-04-22 17:34:11 +0100

    Refactor Flask API endpoints in test.py

--- flask/test.py
+++ flask/test.py
@@ -1,8 +1,17 @@
-from ipinfo import get_ipinfo
+from flask import Flask
+from flask_restx import Api
+from endpoints.system import api as namespaceSystem
+from endpoints.temperature import api as namespaceTemperature
+from endpoints.smoke import api as namespaceSmoke
+from endpoints.rain import api as namespaceRain
 
-def get_location():
-    details = get_ipinfo()
-    return details.city
+app = Flask(__name__)
+api = Api(app, version='1.0', title='Controller Firmware', description='Python Firmware Controller')
 
-city = get_location()
-print("City:", city)
+api.add_namespace(namespaceSystem, path='/system')
+api.add_namespace(namespaceTemperature, path='/temp')
+api.add_namespace(namespaceSmoke, path='/smoke')
+api.add_namespace(namespaceRain, path='/rain')
+
+if __name__ == '__main__':
+    app.run(debug=True)
