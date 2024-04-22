c40b97172b751f5fd66324b011b30ff28de7804f
KyleSeaford <KyleSeaford>
2024-04-22 19:25:42 +0100

    works

--- Pi/firmware.py
+++ Pi/firmware.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from flask_restx import Api
+from flask_cors import CORS
 from endpoints.temperature import api as namespaceTemperature
 from endpoints.smoke import api as namespaceSmoke
 from endpoints.rain import api as namespaceRain
@@ -9,6 +10,7 @@ from endpoints.camera import api as namespaceCamera
 from endpoints.system import api as namespaceSystem
 
 app = Flask(__name__)
+CORS(app)  # Allow CORS for all routes
 api = Api(app, version='1.0', title='Controller Firmware', description='Python Firmware Controller')
 
 api.add_namespace(namespaceSystem, path='/system')
@@ -18,4 +20,4 @@ api.add_namespace(namespaceRain, path='/rain')
 api.add_namespace(namespaceCamera, path='/camera')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='192.168.127.106', port=5000)
