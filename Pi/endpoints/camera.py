881d27f9bd9fdade77581f5809750de4b4dc515b
KyleSeaford <KyleSeaford>
2024-04-29 14:31:07 +0100

    kinda shows a diffrent image

--- Pi/endpoints/camera.py
+++ Pi/endpoints/camera.py
@@ -1,16 +1,18 @@
 from flask import send_file
 from flask_restx import Namespace, Resource
-api = Namespace('Camera', description='camera endpoint')
-
 import random
 import os
 
-image_dir = "/home/robot/FDS/FDS/Pi/endpoints/cameraImgs" 
-images = os.listdir(image_dir)
+api = Namespace('Camera', description='camera endpoint')
+
+
+# random images pls ignore 
+image_dir = "/home/robot/FDS/FDS/Pi/endpoints/cameraImgs"
 
 @api.route('/camera', doc={"description": "take photo on camera"})
 class HelloWorld(Resource):
     def get(self):
+        images = os.listdir(image_dir)
         random_image = random.choice(images)
         image_path = os.path.join(image_dir, random_image)
         return send_file(image_path, mimetype='image/jpeg')
