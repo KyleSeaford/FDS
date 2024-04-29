39ffe921accca4d8e396a7122a852196c369564f
KyleSeaford <KyleSeaford>
2024-04-29 14:44:15 +0100

    lost

--- Pi/endpoints/camera.py
+++ Pi/endpoints/camera.py
@@ -6,12 +6,11 @@ import os
 api = Namespace('Camera', description='camera endpoint')
 
 
-# random images pls ignore 
-image_dir = "/home/robot/FDS/FDS/Pi/endpoints/cameraImgs"
-
 @api.route('/camera', doc={"description": "take photo on camera"})
 class HelloWorld(Resource):
     def get(self):
+        # random images pls ignore 
+        image_dir = "/home/robot/FDS/FDS/Pi/endpoints/cameraImgs"
         images = os.listdir(image_dir)
         random_image = random.choice(images)
         image_path = os.path.join(image_dir, random_image)
