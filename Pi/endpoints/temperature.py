51c3a579315b079d83605a21fcee44347c017f9e
KyleSeaford <kyleseaford5@gmail.com>
2024-05-19 20:37:35 +0100

    added db stuff to sensor temp

--- Pi/endpoints/temperature.py
+++ Pi/endpoints/temperature.py
@@ -4,6 +4,10 @@ api = Namespace('Temperature', description='temp endpoint')
 import os
 import glob
 import time
+import datetime
+import sqlite3
+import threading
+
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
@@ -29,9 +33,50 @@ def read_temp():
         temp_c = float(temp_string) / 1000.0
         return temp_c
 
+# Use a separate thread to run the data addition function
+class DataAdder(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self._stop_event = threading.Event()
 
-@api.route('/temp', doc={"description": "get the temp"})
+    def run(self):
+        while not self._stop_event.is_set():
+            temp = read_temp()
+            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            conn = sqlite3.connect('sensordata.db')
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO Temp (Temp, Time) VALUES (?, ?)', (temp, current_time))
+            conn.commit()
+            conn.close()
+
+            time.sleep(5)
+
+    def stop(self):        
+        self._stop_event.set()
+        print("Data addition stopped")
+
+data_adder = DataAdder()
+data_adder.start()
+
+@api.route('/Temp', doc={"description": "Get the temperature"})
 class HelloWorld(Resource):
     def get(self):
         temp = read_temp()
-        return {'temperature': temp}
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        # Add data to db
+        conn = sqlite3.connect('sensordata.db')
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO Temp (Temp, Time) VALUES (?, ?)', (temp, current_time))
+        conn.commit()
+        conn.close()
+        
+        return {'message': 'Temperature data stored successfully'}
+
+@api.route('/Stop', doc={"description": "Stop the temperature data addition"})
+class Stop(Resource):
+    def get(self):
+        data_adder.stop()
+        data_adder.join()  # Ensure the thread has finished
+        return {'message': 'Temperature data addition stopped successfully'}
